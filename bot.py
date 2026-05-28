f55ffca4871b36bf08d7dfbe89e825cc8d26e381
DrDrozdov <dmiitriidrozdov@gmail.com>
2026-05-28 14:35:03 +0300

    fix railway env

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import os
 import asyncio
-from dotenv import load_dotenv
 
-load_dotenv()
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
